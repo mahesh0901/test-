20286883ddfe0c10063209b6641ab9d29123f198
mahesh0901 <74361994+mahesh0901@users.noreply.github.com>
2020-11-16 12:01:07 +0530

    Add files via upload

---
+++ app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms'
+import { QuizComponent } from './quiz/quiz.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    QuizComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+  
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
